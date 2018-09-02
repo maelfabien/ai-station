b80565eae0dc55d404f3719df7cd3e336bf85823
fmikaelian <39884124+fmikaelian@users.noreply.github.com>
2018-09-02 20:12:05 +0200

    add dockerized tests and CI config to template 🐳+🐍

---
+++ station/template/tests/test_pipeline.py
@@ -0,0 +1,15 @@
+from {{ project_name }}.pipeline.processing import Processing
+from {{ project_name }}.pipeline.train import Train
+from {{ project_name }}.pipeline.predict import Predict
+
+
+def test_processing():
+    assert 'something'
+
+
+def test_train():
+    assert 'something'
+
+
+def test_predict():
+    assert 'something'
